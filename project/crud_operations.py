eed3a116344382ac5db70d00eb1f13aeedd42eac
JonasDeWeerdt <r0879844@students.thomasmore.be>
2023-05-24 18:26:25 +0200

    ok it do be correct now

--- project/crud_operations.py
+++ project/crud_operations.py
@@ -4,11 +4,6 @@ import auth
 import models
 import schemas
 import subprocess
-#import os
-
-
-
-
 
 
 def get_user(db: Session, user_id: int):
@@ -18,14 +13,6 @@ def get_user(db: Session, user_id: int):
 def get_user_by_email(db: Session, email: str):
     return db.query(models.User).filter(models.User.email == email).first()
 
-def deploy_cluster(db:Session,playbook,inventory):
-    try :
-        subprocess.run(['ansible-playbook', playbook, '-i', inventory])
-    except subprocess.CalledProcessError as e:
-        return e.output + "failed"
-    else:
-        return "success"
-    
 
 def create_user(db: Session, user: schemas.UserCreate):
     hashed_password = auth.get_password_hash(user.password)
@@ -33,8 +20,23 @@ def create_user(db: Session, user: schemas.UserCreate):
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
-    return db_user
+    response = {'ansible_user': user.email,
+                'ansible_uid': db.query(models.User).filter(models.User.id == user.id),
+                'ansible_gid': db.query(models.User).filter(models.User.id == user.id),
+                'ansible_password': user.password}
 
+    playbook_path = "ansible/playbook.yml"
+    ansible_command = f"ansible-playbook {playbook_path} --extra-vars="
+    try:
+        # Execute the Ansible playbook command
+        subprocess.run(ansible_command, shell=True, check=True)
+        # Playbook executed successfully
+        return db_user
+    except subprocess.CalledProcessError as e:
+        # Playbook execution failed
+        # Handle the error appropriately (e.g., logging, error response, etc.)
+        # You can access the error message through e.stderr
+        raise Exception(f"Failed to run Ansible playbook: {e.stderr}")
 
 
 def get_users(db: Session, skip: int = 0, limit: int = 100):
@@ -45,7 +47,6 @@ def set_password(db: Session, user: schemas.UserCreate):
     hashed_password = auth.get_password_hash(user.password)
     user_db = db.query(models.User).filter(
         models.User.email == user.email).first()
-    print(user_db)
     if not user_db:
         raise HTTPException(status_code=404, detail="User not found")
 
