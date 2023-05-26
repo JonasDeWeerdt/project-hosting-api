c94b76cf6751935445dc814a75b5cdddbc21f769
JonasDeWeerdt <r0879844@students.thomasmore.be>
2023-05-26 09:31:30 +0200

    pls ansible work

--- project/crud_operations.py
+++ project/crud_operations.py
@@ -30,13 +30,27 @@ def deploy_cluster(db:Session,playbook,inventory):
 def create_user(db: Session, user: schemas.UserCreate):
     hashed_password = auth.get_password_hash(user.password)
     db_user = models.User(email=user.email, hashed_password=hashed_password)
-    #home_dir = "/home/" + user.email
-    #subprocess.run(['sudo', 'useradd', '-m', user.email])
-    #subprocess.run(['sudo', 'chpasswd'], input=f"{user.email}:{user.password}", encoding='utf-8')
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
-    return db_user
+    response = {'ansible_user': user.email,
+                'ansible_uid': 2001,
+                'ansible_gid': 2001,
+                'ansible_password': user.password}
+
+    playbook_path = "ansible_user/playbook.yml"
+    ansible_command = f"ansible-playbook {playbook_path} --extra-vars={response}"
+    try:
+        # Execute the Ansible playbook command
+        subprocess.run(ansible_command, shell=True, check=True)
+        # Playbook executed successfully
+        return db_user
+    except subprocess.CalledProcessError as e:
+        # Playbook execution failed
+        # Handle the error appropriately (e.g., logging, error response, etc.)
+        # You can access the error message through e.stderr
+        raise Exception(f"Failed to run Ansible playbook: {e.stderr}")
+    
 
 
 
